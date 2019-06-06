852b989c1b75c155cacfa28f402e0666fb1f6fd5
wujiusheng <946861549@qq.com>
2019-06-06 08:31:50 +0800

    websocket的广播模式

--- src/main/java/com/hangzhou/springbootdemo/websocket/controller/WsController.java
+++ src/main/java/com/hangzhou/springbootdemo/websocket/controller/WsController.java
@@ -10,10 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class WsController {
 
     @MessageMapping("/welcome") //当浏览器向服务端发送请求时，通过@MessageMapping映射/welcome这个地址，类似于@RequestMapping
-    //如果浏览器订阅了@SendTo中的路径，当服务器有消息时，就会向这些浏览器发送消息
+    //如果浏览器订阅了@SendTo中的路径，当服务器有消息时，就会向这些浏览器发送消息;
     @SendTo("/topic/getResponse")//当服务端有消息时，监听了/topic/getResponse的客户端会接收消息
     public WiselyResponse say(WiselyMessage message) throws Exception {
-        System.out.println("连接成功");
+        System.out.println("连接成功......");
         Thread.sleep(3000);
         return new WiselyResponse("Welcome, " + message.getName() + "!");
     }
