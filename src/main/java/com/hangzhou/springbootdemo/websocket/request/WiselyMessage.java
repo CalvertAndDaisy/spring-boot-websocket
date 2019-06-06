c9392bca186bf0af5619743235d5e036878a2190
wujiusheng <946861549@qq.com>
2019-06-06 08:22:31 +0800

    Initial commit

---
+++ src/main/java/com/hangzhou/springbootdemo/websocket/request/WiselyMessage.java
@@ -0,0 +1,18 @@
+package com.hangzhou.springbootdemo.websocket.request;
+
+/*
+ * 浏览器向服务端发送的消息用此类接收
+ *
+ */
+public class WiselyMessage {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/hangzhou/springbootdemo/websocket/response/WiselyResponse.java
@@ -0,0 +1,22 @@
+package com.hangzhou.springbootdemo.websocket.response;
+
+/*
+ * 服务端向浏览器发送的此类的消息
+ *
+ */
+
+public class WiselyResponse {
+    private String responseMessage;
+
+    public WiselyResponse(String responseMessage) {
+        this.responseMessage = responseMessage;
+    }
+
+    public String getResponseMessage() {
+        return responseMessage;
+    }
+
+    public void setResponseMessage(String responseMessage) {
+        this.responseMessage = responseMessage;
+    }
+}
